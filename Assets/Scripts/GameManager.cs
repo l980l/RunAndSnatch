ed97d8d13f4a1c320ec833e65841b16a344e15e3
l980l <kimkwanhyeon98@gmail.com>
2024-06-09 01:40:13 +0900

    feat: ShadowCaster2D 자동 생성 구현
    랜덤 생성 맵에 ShadowCaster2D를 자동으로 적용하게 하여, 벽 그림자를 구현. 출구로 향하는 길도 4방향 중 랜덤 생성.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject PlayerPrefab;
     [SerializeField] private GameObject[] ItemPrefab;
     [SerializeField] private int ItemCount;
+    [SerializeField] private GameObject[] MonsterPrefabs;
+    [SerializeField] private GameObject ExitPrefab;
 
     private GameObject Player;
     private int TotalItemValue;
@@ -40,20 +42,23 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        // 랜덤한 위치로 플레이어 생성. 초기 HealthHUD 세팅.
-        Player = Instantiate(PlayerPrefab, mapGenerator.RandomPos(false), Quaternion.identity);
-
-        // 시네머신 팔로우 플레이어 트랜스폼으로 세팅.
-        CVC.m_Follow = Player.transform; 
-        // 아이템 생성
-        for(int i = 0; i < ItemCount; i++)
+        if(mapGenerator)
         {
-            int index = Random.Range(0, ItemPrefab.Count());
-            // Item 가치 누적
-            TotalItemValue += ItemPrefab[index].GetComponent<Item>().GetItemValue();
-            Instantiate(ItemPrefab[index], mapGenerator.RandomPos(false), Quaternion.identity);
+            // 랜덤한 위치로 플레이어 생성. 초기 HealthHUD 세팅.
+            Player = Instantiate(PlayerPrefab, mapGenerator.RandomPos(false), Quaternion.identity);
+
+            // 시네머신 팔로우 플레이어 트랜스폼으로 세팅.
+            CVC.m_Follow = Player.transform;
+            // 아이템 생성
+            for (int i = 0; i < ItemCount; i++)
+            {
+                int index = Random.Range(0, ItemPrefab.Count());
+                // Item 가치 누적
+                TotalItemValue += ItemPrefab[index].GetComponent<Item>().GetItemValue();
+                Instantiate(ItemPrefab[index], mapGenerator.RandomPos(false), Quaternion.identity);
+            }
+            // ItemValueText 세팅
+            SetItemValue(0);
         }
-        // ItemValueText 세팅
-        SetItemValue(0);
     }
 }
