caabd4a7c54bf97813517dc2862761b514a69dab
l980l <kimkwanhyeon98@gmail.com>
2024-06-09 01:40:13 +0900

    feat: ShadowCaster2D 자동 생성 구현
    랜덤 생성 맵에 ShadowCaster2D를 자동으로 적용하게 하여, 벽 그림자를 구현. 출구로 향하는 길도 4방향 중 랜덤 생성.

---
+++ Assets/Scripts/ShadowCasterGenerator.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(CompositeCollider2D))]
+public class ShadowCasterGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private bool selfShadows;
+
+    private CompositeCollider2D tilemapCollider;
+
+    static readonly FieldInfo meshField = typeof(ShadowCaster2D).GetField("m_Mesh", BindingFlags.NonPublic | BindingFlags.Instance);
+    static readonly FieldInfo shapePathField = typeof(ShadowCaster2D).GetField("m_ShapePath", BindingFlags.NonPublic | BindingFlags.Instance);
+    static readonly FieldInfo shapePathHashField = typeof(ShadowCaster2D).GetField("m_ShapePathHash", BindingFlags.NonPublic | BindingFlags.Instance);
+    static readonly MethodInfo generateShadowMeshMethod = typeof(ShadowCaster2D).Assembly.GetType("UnityEngine.Rendering.Universal.ShadowUtility").GetMethod("GenerateShadowMesh", BindingFlags.Public | BindingFlags.Static);
+
+    public void Create()
+    {
+        tilemapCollider = GetComponent<CompositeCollider2D>();
+        
+        //DestroyOldShadowCasters();
+
+        for (int i = 0; i < tilemapCollider.pathCount; i++)
+        {
+            Vector2[] pathVertices = new Vector2[tilemapCollider.GetPathPointCount(i)];
+            tilemapCollider.GetPath(i, pathVertices);
+            GameObject shadowCaster = new GameObject("shadow_caster_" + i);
+            shadowCaster.transform.parent = gameObject.transform;
+            ShadowCaster2D shadowCasterComponent = shadowCaster.AddComponent<ShadowCaster2D>();
+            shadowCasterComponent.selfShadows = this.selfShadows;
+
+            Vector3[] testPath = new Vector3[pathVertices.Length];
+            for (int j = 0; j < pathVertices.Length; j++)
+            {
+                testPath[j] = pathVertices[j];
+            }
+
+            shapePathField.SetValue(shadowCasterComponent, testPath);
+            shapePathHashField.SetValue(shadowCasterComponent, UnityEngine.Random.Range(int.MinValue, int.MaxValue));
+            meshField.SetValue(shadowCasterComponent, new Mesh());
+            generateShadowMeshMethod.Invoke(shadowCasterComponent, new object[] { meshField.GetValue(shadowCasterComponent), shapePathField.GetValue(shadowCasterComponent) });
+        }
+    }
+
+    //public void DestroyOldShadowCasters()
+    //{
+    //    // 모든 쉐도우캐스트 transform 저장. 에디터에서 사용할 것이기 때문에 DestroyImmediate. 
+    //    List<Transform> tempList = transform.Cast<Transform>().ToList();
+    //    foreach (var child in tempList)
+    //    {
+    //        DestroyImmediate(child.gameObject);
+    //    }
+    //}
+}
