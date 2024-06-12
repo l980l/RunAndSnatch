c354f35b2a13c558bbbe241a4ccbf5e496b31c8a
l980l <kimkwanhyeon98@gmail.com>
2024-06-12 12:32:03 +0900

    perf: AccoutData에 아이템 정보 ItemType으로 저장, JSON 인코딩 및 암호화해서 저장

--- Assets/Scripts/Manager/AccountDataManager.cs
+++ Assets/Scripts/Manager/AccountDataManager.cs
@@ -1,19 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
+using System.Threading.Tasks;
 
 [System.Serializable]
 public class AccountData    // 캐릭터 해금 정보, 캐릭터 별 선물 정보도 추가해야 한다.
 {
-    public Item[] Items;
+    public ItemType[] Items;
     public int gold;
 }
 
 public class AccountDataManager : MonoBehaviour
 {
-    // 싱글톤으로 지정.DontDestroyOnLoad로 지정.
     #region Singleton
     public static AccountDataManager Instance;
     private void Awake()
@@ -29,9 +28,20 @@ public class AccountDataManager : MonoBehaviour
     #endregion
 
     private string filePath;
+    private string keyWord = "341#@sdf^&gr$w&bk`9";
     private AccountData accountData;
-    public int AccountGold { get { return accountData.gold; } set { accountData.gold = value; SaveJson(); } }
-    public Item[] GetAccountInven() { return accountData.Items; }
+    private readonly object fileLock = new object(); // 파일 쓰기 동기화를 위한 객체
+
+    public int AccountGold
+    {
+        get { return accountData.gold; }
+        set
+        {
+            accountData.gold = value;
+            SaveJsonAsync();
+        }
+    }
+    public ItemType[] GetAccountInven() { return accountData.Items; }
 
     private void Start()
     {
@@ -41,30 +51,56 @@ public class AccountDataManager : MonoBehaviour
 
     public void UpdateAccountItems(List<Item> _items)
     {
-        accountData.Items = _items.ToArray();
-        SaveJson();
+        List<ItemType> temp = new List<ItemType>();
+        foreach (Item item in _items)
+        {
+            temp.Add(item.ItemType);
+        }
+        accountData.Items = temp.ToArray();
+        SaveJsonAsync();
+    }
+
+    private async void SaveJsonAsync()
+    {
+        await Task.Run(() => SaveJson());
     }
 
     private void SaveJson()
     {
-        string jsonData = JsonUtility.ToJson(accountData);
-        Debug.Log(jsonData);
-        File.WriteAllText(filePath, jsonData);
+        lock (fileLock)
+        {
+            string jsonData = JsonUtility.ToJson(accountData);
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(jsonData);
+            string code = System.Convert.ToBase64String(bytes);
+            string encryptedData = EncryptAndDecript(code);
+            File.WriteAllText(filePath, encryptedData);
+        }
     }
 
     private void LoadJson()
     {
-        // 저장된 정보가 있는 경우에만 로딩. 
-        if(File.Exists(filePath))
+        if (File.Exists(filePath))
         {
-            string jsonData = File.ReadAllText(filePath);
+            string code = File.ReadAllText(filePath);
+            byte[] bytes = System.Convert.FromBase64String(EncryptAndDecript(code));
+            string jsonData = System.Text.Encoding.UTF8.GetString(bytes);
             accountData = JsonUtility.FromJson<AccountData>(jsonData);
         }
-
-        // 없으면 초기 값을 세팅해줘야 함.
         else
         {
             accountData = new AccountData();
         }
+    }
+
+    private string EncryptAndDecript(string data)
+    {
+        string result = "";
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            result += (char)(data[i] ^ keyWord[i % keyWord.Length]);
+        }
+
+        return result;
     }
 }
