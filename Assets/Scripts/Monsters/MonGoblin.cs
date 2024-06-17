39611da51e0cc71d2d0454e96fefc6f8d2b5390e
l980l <kimkwanhyeon98@gmail.com>
2024-06-17 21:11:43 +0900

    feat: Goblin FSM 완성

--- Assets/Scripts/Monsters/MonGoblin.cs
+++ Assets/Scripts/Monsters/MonGoblin.cs
@@ -1,9 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class MonGoblin : Monster
 {
-   
+    [SerializeField] private BoxCollider2D atkBox;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        atkBox.enabled = false;
+    }
+
+    protected override void Attack()
+    {
+        if (atkBox != null)
+        {
+            FlipCollider(GetComponent<SpriteRenderer>().flipX);
+            atkBox.enabled = true;
+            StartCoroutine(DisableAtkBox(0.1f));
+        }
+    }
+
+    private IEnumerator DisableAtkBox(float _delay)
+    {
+        yield return new WaitForSeconds(_delay);
+        if (atkBox != null)
+        {
+            atkBox.enabled = false;
+        }
+    }
+
+    protected void FlipCollider(bool _FlipX)
+    {
+        if(atkBox != null)
+        {
+            if(_FlipX)
+            {
+                if (atkBox.offset.x > 0)
+                {
+                    Vector3 colliderPos = atkBox.offset;
+                    colliderPos.x = -colliderPos.x;
+                    atkBox.offset = colliderPos;
+                }
+            }
+            else
+            {
+                if (atkBox.offset.x < 0)
+                {
+                    Vector3 colliderPos = atkBox.offset;
+                    colliderPos.x = -colliderPos.x;
+                    atkBox.offset = colliderPos;
+                }
+            }
+        }
+    }
 }
