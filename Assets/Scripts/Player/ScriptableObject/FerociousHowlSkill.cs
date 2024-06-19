2956e83dd62e8062d30491ebbc6491cff7427882
l980l <kimkwanhyeon98@gmail.com>
2024-06-19 18:19:18 +0900

    feat: PlayerData, SkillEffect 추가
    각 스킬별로 SkillEffect 추가 했고, PlayerData는 하나로 모든 캐릭터를 표현 하는 구조. Player에서 PlayerData를 사용하도록 수정.

---
+++ Assets/Scripts/Player/ScriptableObject/FerociousHowlSkill.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/SkillSO/FerociousHowlSkill")]
+public class FerociousHowlSkill : SkillEffect
+{
+    public override bool ExecuteRole()
+    {
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Player/ScriptableObject/PlayerData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterType
+{
+    Miya,
+    Bambi,
+    Leo,
+    Cosmo,
+    Chrono,
+    Misty
+}
+
+[CreateAssetMenu(menuName = "ScriptableObject/PlayerData")] 
+public class PlayerData : ScriptableObject
+{
+    public CharacterType characterType;
+    public int maxHP;
+    public float maxStamina;
+    public float staminaRegenSpeed;
+    public float speed;
+    public float dodgeSpeed;
+    public SkillEffect skill;
+}
