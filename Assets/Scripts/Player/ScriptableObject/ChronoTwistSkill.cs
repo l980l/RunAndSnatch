8623d635fb8fb8707428927f3e2e480769c9597c
l980l <kimkwanhyeon98@gmail.com>
2024-06-19 18:19:18 +0900

    feat: PlayerData, SkillEffect 추가
    각 스킬별로 SkillEffect 추가 했고, PlayerData는 하나로 모든 캐릭터를 표현 하는 구조. Player에서 PlayerData를 사용하도록 수정.

---
+++ Assets/Scripts/Player/ScriptableObject/ChronoTwistSkill.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/SkillSO/ChronoTwistSkill")]
+public class ChronoTwistSkill : SkillEffect
+{
+    public override bool ExecuteRole()
+    {
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Player/ScriptableObject/SpatialWarpSkill.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/SkillSO/SpatialWarpSkill")]
+public class SpatialWarpSkill : SkillEffect
+{
+    public override bool ExecuteRole()
+    {
+
+        return true;
+    }
+}
