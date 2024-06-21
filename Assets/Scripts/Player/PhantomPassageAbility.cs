ff65463be2482c7edb1ca468ef1f0eaa9230ca18
l980l <kimkwanhyeon98@gmail.com>
2024-06-22 04:55:24 +0900

    feat: PhantomPassage(벽 통과) 스킬 구현

--- Assets/Scripts/Player/PhantomPassageAbility.cs
+++ Assets/Scripts/Player/PhantomPassageAbility.cs
@@ -6,11 +6,18 @@ using UnityEngine.Rendering.Universal;
 public class PhantomPassageAbility : MonoBehaviour
 {
     private Player player;
+    private Rigidbody2D rb;
     [SerializeField] private Light2D mistyLight;
+    private MapGenerator mapGenerator;
+    private Grid grid;
 
     private void Start()
     {
         player = GetComponent<Player>();
+        rb = GetComponent<Rigidbody2D>();
+        mapGenerator= GameManager.Instance.GetMapGenerator();
+        if(mapGenerator)
+            grid = mapGenerator.GetGrid();
     }
 
     public void UsePhantomPassage(float duration)
@@ -31,5 +38,24 @@ public class PhantomPassageAbility : MonoBehaviour
         gameObject.layer = 6;
         // mistyLight 끄기
         mistyLight.enabled = false;
+
+        // 탈출
+        StartCoroutine(TeleportCoroutine());
+    }
+
+    private IEnumerator TeleportCoroutine()
+    {
+        // 위치 이동
+        player.onMovingSkill = true;
+        Vector3Int nowPos = grid.WorldToCell(transform.position);
+        // 현재 위치가 벽인 경우. 가장 가까운 Road 찾고, Cell 중간 길이 더해주고.
+        if (mapGenerator.isWallAtPos(nowPos.x, nowPos.y) == true)
+        {
+            rb.MovePosition(grid.CellToWorld((Vector3Int)mapGenerator.GetNearestRoad(nowPos.x, nowPos.y)) + grid.GetLayoutCellCenter());
+        }
+
+        yield return new WaitForFixedUpdate();
+
+        player.onMovingSkill = false;
     }
 }
