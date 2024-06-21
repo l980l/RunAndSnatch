977b5528dcd547a744083a51495a4d6fff8a242c
l980l <kimkwanhyeon98@gmail.com>
2024-06-22 03:21:36 +0900

    fix: PhantomPassage 중에 달리기로 벽 넘기 가능하게 수정
    더이상 Invincible Layer를 사용해서 Player의 무적을 판단하지 않고, invincible 변수를 이용한다.

--- Assets/Scripts/Player/PhantomPassageAbility.cs
+++ Assets/Scripts/Player/PhantomPassageAbility.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PhantomPassageAbility : MonoBehaviour
 {
     private Player player;
+    [SerializeField] private Light2D mistyLight;
 
     private void Start()
     {
@@ -20,10 +22,14 @@ public class PhantomPassageAbility : MonoBehaviour
     {
         // Phantom 레이어로
         gameObject.layer = 14;
+        // mistyLight 밝히기
+        mistyLight.enabled = true;
 
         yield return new WaitForSecondsRealtime(duration);
 
         // 끝나면 Player 레이어로
         gameObject.layer = 6;
+        // mistyLight 끄기
+        mistyLight.enabled = false;
     }
 }
