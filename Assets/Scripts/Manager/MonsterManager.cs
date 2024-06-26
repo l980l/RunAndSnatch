761fbca245657094d24ddd3672e9177543789c2d
l980l <kimkwanhyeon98@gmail.com>
2024-06-26 23:51:52 +0900

    chore: MonsterSpawnRatio 수정
    몬스터 스폰 비율을 하나만 사용하도록 수정.

--- Assets/Scripts/Manager/MonsterManager.cs
+++ Assets/Scripts/Manager/MonsterManager.cs
@@ -4,16 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public enum DifLevel
-{
-    ChickenHeart,
-    Spooky,
-    CreepyCrawler,
-    Nightmare,
-    Hellbound,
-    Max
-}
-
 public class MonsterManager : MonoBehaviour
 {
     public static MonsterManager Instance;
@@ -25,7 +15,6 @@ public class MonsterManager : MonoBehaviour
     [Tooltip("FlyingEye, Goblin, EvilWizard, Skeleton")]
     [SerializeField] private GameObject[] monsterPrefab;
     [SerializeField] private int monsterCount;
-    [SerializeField] private DifLevel difficulty;
     [SerializeField] private MonsterSpawnRatio MonsterRatio;
 
 
@@ -38,7 +27,7 @@ public class MonsterManager : MonoBehaviour
             // 몬스터 종류별로 for문
             for (int j = 0; j < (int)MonsterType.Max; j++)
             {
-                cumulativeRatio += MonsterRatio.monsterRatio[(int)difficulty][j];
+                cumulativeRatio += MonsterRatio.monsterRatio[j];
                 if (randomValue < cumulativeRatio)
                 {
                     Instantiate(monsterPrefab[j], GameManager.Instance.GetMapGenerator().RandomPos(false), Quaternion.identity);
--- Assets/Scripts/Monsters/MonsterSpawnRatio.cs
+++ Assets/Scripts/Monsters/MonsterSpawnRatio.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObject/MonsterSpawnRatio")]
 public class MonsterSpawnRatio : ScriptableObject
 {
-    // 난이도 0부터 4까지
-    public List<List<int>> monsterRatio = new List<List<int>>();
+    public List<int> monsterRatio = new List<int>();
 }
