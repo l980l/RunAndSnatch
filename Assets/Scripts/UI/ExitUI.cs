ca89313a57a40c726607ba13503994d80e49610a
l980l <kimkwanhyeon98@gmail.com>
2024-06-27 07:09:30 +0900

    feat: 사망 UI, 타이머 UI, 탈출구 스크립트, 탈출 UI 구현
    타이머 값은 300초. 다되면 사망.
    
    사망 시 사망 UI 출력. 2초 후 마을로 이동.
    
    사망 패널티 적용 함수는 AccountDataManager에 구현. 사망 시 호출.
    
    탈출구에 갔을 때, 아이템 70프로 이상 획득 시 탈출 가능. 탈출 시 마을이나 다음 던전으로 이동 버튼.
    
    던전에서 게임 종료시, 사망 패널티 적용.

---
+++ Assets/Scripts/UI/ExitUI.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitUI : MonoBehaviour
+{
+    #region singletone
+    public static ExitUI instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    private PlayerItemCollect playerItemCollect;
+    [SerializeField] private GameObject cantExitText;
+    [SerializeField] private GameObject exitUIPanel;
+    [SerializeField] private Text exitStreakText;
+    [SerializeField] private Text exitTimeText;
+    [SerializeField] private Text collectedItemText;
+    [SerializeField] private TimerUI timerUI;
+
+    [SerializeField] private int DungeonSceneInt;
+    [SerializeField] private int CatTownSceneInt;
+
+    private void Start()
+    {
+        cantExitText.SetActive(false);
+        exitUIPanel.SetActive(false);
+    }
+
+    public void OpenExitUI()
+    {
+        if (playerItemCollect == null)
+        {
+            playerItemCollect = GameManager.Instance.GetPlayer().GetComponent<PlayerItemCollect>();
+        }
+
+        // 아이템 70프로 이상 획득.
+        if (playerItemCollect.AcquiredItemCount >= ItemDB.Instance.GetTotalItemCount() * 0.7f)
+        {
+            // 연속 탈출 횟수, 던전 여부 업데이트
+            AccountDataManager.Instance.ExitStreak++;
+            AccountDataManager.Instance.InDungeon = false;
+            AccountDataManager.Instance.SaveJsonToCloud();
+
+            exitUIPanel.SetActive(true);
+            Time.timeScale = 0f;
+
+            exitStreakText.text = "Exit Streak: " + AccountDataManager.Instance.ExitStreak.ToString();
+            exitTimeText.text = "Time Taken: " + (timerUI.FullTime -timerUI.RemainTime).ToString();
+            collectedItemText.text = "Snatched Item Count: " + playerItemCollect.AcquiredItemCount.ToString();
+        }
+
+        // 2초간 아이템 더 가져와야 된다는 텍스트 띄우기.
+        else
+        {
+            ShowCantExitText();
+        }
+    }
+
+    private void ShowCantExitText()
+    {
+        StartCoroutine(ShowCantExitTextCoroutine());
+    }
+
+    private IEnumerator ShowCantExitTextCoroutine()
+    {
+        cantExitText.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        cantExitText.SetActive(false);
+    }
+
+    public void NextDungeonButtonClick()
+    {
+        Time.timeScale = 1f;
+        LoadingSceneController.LoadScene(DungeonSceneInt);
+    }
+
+    public void ReturnToTownButtonClick()
+    {
+        Time.timeScale = 1f;
+        LoadingSceneController.LoadScene(CatTownSceneInt);
+    }
+}
