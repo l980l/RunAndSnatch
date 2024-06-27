bc3a47ea57a16e9e7ad79f7118bdecf751bfa781
l980l <kimkwanhyeon98@gmail.com>
2024-06-27 17:07:30 +0900

    feat: 충돌로 씬 이동

---
+++ Assets/Scripts/SceneLaod/Tips.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Tips")] 
+public class Tips : ScriptableObject
+{
+    public string[] EnTips;
+    public string[] KrTips;
+}
