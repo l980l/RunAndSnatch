2a8a271e4b565f07f9f7175b95e983a5b6e0e9c7
l980l <kimkwanhyeon98@gmail.com>
2024-07-04 12:41:53 +0900

    feat: PixelPerfectSetting 스크립트 추가
    Pixel Perfect Camera의 Reference Resolution을 자동으로 세팅하도록 스크립트 추가.

--- Assets/Scripts/Camera/PixelPerfectSetting.cs
+++ Assets/Scripts/Camera/PixelPerfectSetting.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class PixelPerfectSetting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PixelPerfectCamera pixelPerfectCamera;
+
     void Start()
     {
-        
-    }
+        pixelPerfectCamera = GetComponent<PixelPerfectCamera>();
+        if (pixelPerfectCamera == null)
+            return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // 현재 화면 해상도의 1/6로 세팅
+        int screenWidth = Screen.width / 6;
+        int screenHeight = Screen.height / 6;
+
+        pixelPerfectCamera.refResolutionX = screenWidth;
+        pixelPerfectCamera.refResolutionY = screenHeight;
+
+        // Pixel Perfect Camera 설정을 적용
+        pixelPerfectCamera.enabled = false;
+        pixelPerfectCamera.enabled = true;
     }
 }
