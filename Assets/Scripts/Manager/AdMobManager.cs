4436b6876036c4250e1810f37ae94f632e59968b
l980l <kimkwanhyeon98@gmail.com>
2024-07-11 22:22:53 +0900

    feat: AdsMob 적용
    씬 이동시 배너 광고
    사망시 리워드 광고로 패널티 없애기 가능
    사망 테스트를 위해, 데미지 들어오게 수

--- Assets/Scripts/Manager/AdMobManager.cs
+++ Assets/Scripts/Manager/AdMobManager.cs
@@ -1,18 +1,105 @@
+using GoogleMobileAds.Api;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class AdMobManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Singleton
+    public static AdMobManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    [SerializeField] private bool testMode;
+    public Action onRewardedAdFinished = null;
+
     void Start()
     {
-        
+        var requestConfiguration = new RequestConfiguration
+           .Builder()
+           .SetTestDeviceIds(new List<string>() { "b7ffcbadb3204c26" }) // test Device ID
+           .build();
+
+        MobileAds.SetRequestConfiguration(requestConfiguration);
+
+        LoadBannerAd();
+        LoadFrontAd();
+        LoadRewardAd();
     }
 
-    // Update is called once per frame
-    void Update()
+    private AdRequest GetAdRequest()
     {
-        
+        return new AdRequest.Builder().Build();
     }
+
+    #region Banner Ad
+    const string bannerTestID = "ca-app-pub-3940256099942544/6300978111";
+    const string bannerID = "ca-app-pub-3030555614869163/3261602205";
+    BannerView bannerAd;
+
+    private void LoadBannerAd()
+    {
+        bannerAd = new BannerView(testMode ? bannerTestID : bannerID,
+            AdSize.SmartBanner, AdPosition.Top);
+        bannerAd.LoadAd(GetAdRequest());
+        ToggleBannerAd(false);
+    }
+
+    public void ToggleBannerAd(bool b)
+    {
+        if (b) bannerAd.Show();
+        else bannerAd.Hide();
+    }
+    #endregion
+
+    #region Front Ad
+    const string frontTestID = "ca-app-pub-3940256099942544/8691691433";
+    const string frontID = "ca-app-pub-3030555614869163/5911499047";
+    InterstitialAd frontAd;
+
+    void LoadFrontAd()
+    {
+        frontAd = new InterstitialAd(testMode ? frontTestID : frontID);
+        frontAd.LoadAd(GetAdRequest());
+    }
+
+    public void ShowFrontAd()
+    {
+        frontAd.Show();
+        LoadFrontAd();
+    }
+    #endregion
+
+    #region 리워드 광고
+    const string rewardTestID = "ca-app-pub-3940256099942544/5224354917";
+    const string rewardID = "ca-app-pub-3030555614869163/2195932541";
+    RewardedAd rewardAd;
+
+    private void LoadRewardAd()
+    {
+        rewardAd = new RewardedAd(testMode ? rewardTestID : rewardID);
+        rewardAd.LoadAd(GetAdRequest());
+        // 리워드 광고 시청 완료
+        rewardAd.OnUserEarnedReward += (sender, e) =>
+        {
+            onRewardedAdFinished?.Invoke();
+        };
+    }
+
+    public void RewardButtonClick()
+    {
+        rewardAd.Show();
+        LoadRewardAd();
+    }
+    #endregion
 }
